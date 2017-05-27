667e7e765a0562891f4100d1c2df8fbe15b05c7e
xhihi <10148390@qq.com>
2017-05-27 21:15:22 +0800

    完成遍历省市县三级列表的功能

---
+++ app/src/main/java/com/flyweather/android/util/HttpUtil.java
@@ -0,0 +1,16 @@
+package com.flyweather.android.util;
+
+import okhttp3.OkHttpClient;
+import okhttp3.Request;
+
+/**
+ * Created by xh on 2017/5/27.
+ */
+
+public class HttpUtil {
+    public static void sendOkHttpRequest(String address, okhttp3.Callback callback) {
+        OkHttpClient client = new OkHttpClient();
+        Request request = new Request.Builder().url(address).build();
+        client.newCall(request).enqueue(callback);
+    }
+}
